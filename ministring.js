e1466afb2ac11e9a35d8dbd25a2205ec1c6d7a55
Lucas Almeida <code.moz.00@gmail.com>
2020-02-29 14:23:37 +0000

    can select length and character type for string

--- ministring.js
+++ ministring.js
@@ -19,27 +19,47 @@ const characterTypes = [
 	}
 ];
 
-// gets a random value between two numbers (min, max)
-function randomIntFromInterval(min, max) {
-	// min and max included
+// gets a random value between two numbers (min and max included)
+const randomIntFromInterval = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1) + min);
-}
+};
 
-const generate = length => {
-	if (!length) return { Error: 'You must provide a length!' };
+// filters the characterTypes array and returns
+// the character types the user wants
+const parseCharacterTypes = obj => {
+	const selectedCharacters = obj.characters
+		.toString()
+		.replace(/\s+/g, '')
+		.split(',');
+
+	const types = [];
+
+	selectedCharacters.forEach(charType =>
+		types.push(characterTypes.filter(characterType => characterType.type === charType)[0])
+	);
+
+	return types;
+};
+
+const generate = (obj = { characters: 'number, lowercase, uppercase', length: 6 }) => {
+	if (!obj.characters) obj.characters = 'number, lowercase, uppercase';
+	if (!obj.length) obj.length = 6;
+
+	console.log(obj);
 
 	// array which will store all random characters
 	const randomChars = [];
+	const availableCharactersTypes = parseCharacterTypes(obj);
 
 	// get n characters
-	for (let i = 0; i < length; i++) {
+	for (let i = 0; i < obj.length; i++) {
 		// ---------------------------------------
 		// chooses a random number between 0 and 2
-		const tmpIndex = randomIntFromInterval(0, 2);
+		const tmpIndex = randomIntFromInterval(0, availableCharactersTypes.length - 1);
 
 		// selects value type from characterTypes array
 		// through the given index
-		const tmpCharacterType = characterTypes[tmpIndex];
+		const tmpCharacterType = availableCharactersTypes[tmpIndex];
 		const tmp = randomIntFromInterval(tmpCharacterType.start, tmpCharacterType.end);
 		if (tmpCharacterType.type !== 'number') {
 			randomChars.push(String.fromCharCode(tmp));
@@ -51,9 +71,7 @@ const generate = length => {
 	// parse array to string
 	const parsedString = randomChars.toString().replace(/,/g, '');
 
-	// return parsedString;
-	console.log(parsedString);
-	console.log('This is a log 😋');
+	return parsedString;
 };
 
 exports.generate = generate;
