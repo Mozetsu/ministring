f8c78e0433664e8060cfa84794f6209fe0a5a49d
Mozetsu <code.moz.00@gmail.com>
2020-06-17 23:27:38 +0100

    implementing crypto module

--- ministring.js
+++ ministring.js
@@ -1,20 +1,37 @@
-const numbers = '0123456789'.split('');
-const lowercase = 'abcdefghijklmnopqrstuvwxyz'.split('');
-const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const random = (length = 6) => {
+	require('crypto').randomBytes(256, (err, buf) => {
+		if (err) throw new err();
+		a = buf.toString('hex').slice(0, length);
+	});
+	return a;
+};
+console.log(random(30));
 
-function randomIntFromInterval(min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function randomIntFromInterval(min, max) {
+// 	return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
-function generate(length = 5, characters = { numbers, lowercase, uppercase }) {
-	const selectedCharacters = Object.entries(characters);
-	const randomString = [];
-	for (let i = 0; i < length; i++) {
-		const randomCharacterType = randomIntFromInterval(0, selectedCharacters.length - 1);
-		const randomChar = randomIntFromInterval(0, selectedCharacters[randomCharacterType][1].length - 1);
-		randomString.push(selectedCharacters[randomCharacterType][1][randomChar]);
-	}
-	return randomString.toString().replace(/,/g, '');
-}
+// function ministring(length = 5, characters = 'numbers, lowercase, uppercase') {
+// 	const allowedCharacters = {
+// 		specials: '!@#$%^&*?'.split(''),
+// 		numbers: '0123456789'.split(''),
+// 		lowercase: 'abcdefghijklmnopqrstuvwxyz'.split(''),
+// 		uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+// 	};
 
-module.exports = { generate };
+// 	const selectedCharacters = characters.replace(/ /g, '').split(',');
+// 	const verifiedCharacters = [];
+// 	for (const character of selectedCharacters) {
+// 		if (!Object.keys(allowedCharacters).includes(character)) return { error: 'Character type not allowed!' };
+// 		verifiedCharacters.push(...allowedCharacters[character]);
+// 	}
+
+// 	const randomString = [];
+// 	for (let i = 0; i < length; i++) {
+// 		const x = randomIntFromInterval(0, verifiedCharacters.length - 1);
+// 		randomString.push(verifiedCharacters[x]);
+// 	}
+// 	return randomString.toString().replace(/,/g, '');
+// }
+
+// module.exports = ministring;
